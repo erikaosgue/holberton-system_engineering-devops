5ccc174b6baac8af0d8d40850bd3548eab02bb6a
erikaosgue <1476@holbertonschool.com>
2020-07-06 23:55:20 +0000

    Task 6: Hotfix

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,7 +1,3 @@
 #!/usr/bin/env ruby
 
-
-if ARGV.length == 1
-    puts ARGV[0].scan(/^\d{10,10}/).join("")
-    # puts ARGV[0].scan(/^[0-9]{10,10}/).join("")
-end
+puts ARGV[0].scan(/^[0-9]{10}$/).join
