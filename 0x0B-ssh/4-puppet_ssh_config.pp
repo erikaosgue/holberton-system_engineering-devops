b7d424df004f6d7f43c07baf4cee2a8418b6b5f3
erikaosgue <1476@holbertonschool.com>
2020-08-04 20:11:35 +0000

    Updae task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,17 @@
-exec { 'Add text to /etc/ssh/ssh_config':
-  # path    => '/etc/ssh/ssh_config',
-  path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-  command => 'echo "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" | sudo tee -a /etc/ssh/ssh_config',
+# using Puppet to make changes to our configuration file
+
+file_line { 'Turn off passwd auth':
+  ensure             => present,
+  path               => '/etc/ssh/ssh_config',
+  match              => '#  PasswordAuthentication yes',
+  line               => '   PasswordAuthentication no',
+  append_on_no_match => true,
+}
+
+file_line { 'Declare identity file':
+  ensure             => present,
+  path               => '/etc/ssh/ssh_config',
+  match              => '   IdentityFile ~/.ssh/holberton',
+  line               => '   IdentityFile ~/.ssh/holberton',
+  append_on_no_match => true,
 }
