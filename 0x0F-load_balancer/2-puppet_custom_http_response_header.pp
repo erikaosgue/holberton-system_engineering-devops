4f682071a3be16cb5aa23a5eb2db52650d96ae04
erikaosgue <1476@holbertonschool.com>
2020-08-15 04:36:54 +0000

    Add task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,18 @@
 # Add a custom HTTP header with Puppet manifest:
 
-exec { '/usr/bin/env apt-get update -y':
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
+  ensure => installed,
 }
-package { 'nginx':
-    ensure => installed,
+-> file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
 }
-file { '/var/www/html/index.html':
-    content => 'Holberton School!',
+-> file_line { 'add header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'listen [::]:80 default_server;',
 }
-file_line { 'add header':
-    ensure => present,
-    path   => '/etc/nginx/sites-available/default',
-    line   => '\tadd_header X-Served-By $HOSTNAME;',
-    after  => 'listen [::]:80 default_server;',
-}
-service { 'nginx':
-    ensure => running,
+-> service { 'nginx':
+  ensure => running,
 }
