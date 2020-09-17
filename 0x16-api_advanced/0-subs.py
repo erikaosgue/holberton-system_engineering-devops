c9adcdd842434d1ee63eaeb07529afaca0098c3c
erikaosgue <1476@holbertonschool.com>
2020-09-17 22:52:44 +0000

    Add task 0:  How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    resp = requests.get(url, allow_redirects=False, headers={
+                        'User-agent': 'erika hbtn 1.0'})
+    if resp.ok:
+        results = resp.json().get("data")
+        print(results)
+        return results.get("subscribers")
+    return 0
