e4a702ad1c34209e11a320717bbd2fe3394df95f
erikaosgue <1476@holbertonschool.com>
2020-09-30 22:26:17 +0000

    Update task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix error server 500 for wordpress, having bad extension <phpp> to <php> in <wp-settings.php> file
 exec { 'fix-wordpress':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path => 'usr/local/bin/:/bin/'
+    path    => '/usr/bin:/sbin:/bin:/usr/sbin'
 }
